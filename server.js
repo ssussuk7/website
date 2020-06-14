fa40236b80fcf3799e30f765316c22bfa0593b6c
ssussuk7 <31760255+ssussuk7@users.noreply.github.com>
2020-06-15 07:14:46 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+function send404Message(response){
+  response.writeHead(404,{"Content-Type":"text/plain"});
+  response.write("404")
+  response.end()
+}
+
+function onRequest(request, response){
+
+  if(request.method == 'GET' && request.url == '/'){
+    response.writeHead(200,{"Content-Type":"text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+    } else {
+      send404Message(response);
+
+  }
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server Created...");​
